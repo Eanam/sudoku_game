9ad39e457da921b2e7555023ece29a4653ee616d
YinanChenAlso <yinan@falcons.com>
2024-10-01 22:54:47 +0400

    create responsive sudoku panel

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ import SudokuPanel from './components/SudokuPanel';
 function App() {
   return (
     <div className="App">
-        <SudokuPanel />
+      <div className='CenterBox'>
+        <div className='PannelGrid'>
+          {
+            Array.from({ length: 9 }, (_, index) => (
+              <SudokuPanel  className={"PannelItem"} key={index}/>
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/SudokuPanel.tsx
+++ src/components/SudokuPanel.tsx
@@ -1,28 +1,34 @@
 import { TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
 
-function SudokuPanel() {
+interface SudokuPanelProps {
+    className?: string
+}
+
+function SudokuPanel(props: SudokuPanelProps) {
 
     //parse items form props
     let items = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
     return (
-        <TableContainer>
-            <TableBody>
-                {
-                    items.map((row) => (
-                        <TableRow>
-                            {
-                                row.map((item) => (
-                                    <TableCell>
-                                        {item}
-                                    </TableCell>
-                                ))
-                            }
-                        </TableRow>
-                    ))
-                }
-            </TableBody>
-        </TableContainer>
+        <div className={props.className}>
+            <TableContainer>
+                <TableBody>
+                    {
+                        items.map((row) => (
+                            <TableRow>
+                                {
+                                    row.map((item) => (
+                                        <TableCell>
+                                            {item}
+                                        </TableCell>
+                                    ))
+                                }
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </TableContainer>
+        </div>
     );
 
 }
